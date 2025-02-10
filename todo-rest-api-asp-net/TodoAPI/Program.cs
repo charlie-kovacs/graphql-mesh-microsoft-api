cb6d583e559677e1e3132e46e9bb892a049b1b9c
Charlie Kovacs <ckovacs@ssnc-corp.global>
2025-02-10 13:22:35 -0600

    change asp.net API paths to match the Express project paths

--- todo-rest-api-asp-net/TodoAPI/Program.cs
+++ todo-rest-api-asp-net/TodoAPI/Program.cs
@@ -7,24 +7,24 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
-app.MapGet("/todoitems", async (TodoDb db) => await db.Todos.ToListAsync());
+app.MapGet("/todos", async (TodoDb db) => await db.Todos.ToListAsync());
 
-app.MapGet("/todoitems/complete", async (TodoDb db) => await db.Todos.Where(t => t.Completed).ToListAsync());
+app.MapGet("/todos/complete", async (TodoDb db) => await db.Todos.Where(t => t.Completed).ToListAsync());
 
-app.MapGet("/todoitems/{id}", async (int id, TodoDb db) =>
+app.MapGet("/todos/{id}", async (int id, TodoDb db) =>
     await db.Todos.FindAsync(id)
         is Todo todo
             ? Results.Ok(todo)
             : Results.NotFound());
 
-app.MapPost("/todoitems", async (Todo todo, TodoDb db) =>
+app.MapPost("/todos", async (Todo todo, TodoDb db) =>
 {
     db.Todos.Add(todo);
     await db.SaveChangesAsync();
     return Results.Created($"/todoitems/{todo.Id}", todo);
 });
 
-app.MapPut("/todoitems/{id}", async (int id, Todo inputTodo, TodoDb db) =>
+app.MapPut("/todos/{id}", async (int id, Todo inputTodo, TodoDb db) =>
 {
     var todo = await db.Todos.FindAsync(id);
     if (todo is null) return Results.NotFound();
@@ -34,7 +34,7 @@ app.MapPut("/todoitems/{id}", async (int id, Todo inputTodo, TodoDb db) =>
     return Results.NoContent();
 });
 
-app.MapDelete("/todoitems/{id}", async (int id, TodoDb db) =>
+app.MapDelete("/todos/{id}", async (int id, TodoDb db) =>
 {
     if (await db.Todos.FindAsync(id) is Todo todo)
     {
